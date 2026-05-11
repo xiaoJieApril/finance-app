23d4cb5f58add1522dd7152016f82b9969823271
JunJie <tanjun454@gmail.com>
2026-05-11 22:25:54 +0800

    UI completed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,5 @@
+import '../global.css';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
 import React from 'react';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = function (api) {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"], // 新增這一行
-  };
-};
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  presets: [require("nativewind/preset")], // <--- 就是少了這一行！
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
